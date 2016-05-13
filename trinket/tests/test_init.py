513db7a135590d9ad9cabc17ba3128383de32328
Benjamin Bengfort <benjamin@bengfort.com>
2016-05-13 11:14:52 -0400

    update heroku runtime

--- trinket/tests/test_init.py
+++ trinket/tests/test_init.py
@@ -23,7 +23,7 @@ import unittest
 ## Initialization Tests (for sanity)
 ##########################################################################
 
-EXPECTED_VERSION = "0.2"
+EXPECTED_VERSION = "0.2.1"
 
 class InitializationTests(unittest.TestCase):
 
