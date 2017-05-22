c75f4a084944a0ed7b3b44076925b2add80f9ecc
lauralorenz <llorenz@districtdatalabs.com>
2017-05-21 22:27:11 -0700

    add service checking to django entrypoint since runserver won't rekick on database connection errors. requires psql in the django container. questioning why we're trying to dealing with \r line endings, but only for entrypoints

--- compose/django/entrypoint.sh
+++ compose/django/entrypoint.sh
@@ -16,4 +16,11 @@ fi
 
 export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@postgres:5432/$POSTGRES_DB
 
+until psql -h postgres -U "postgres" -c '\l'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+
 exec "$@"
