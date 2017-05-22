6b04931bb9d744e8f55dd79d1b2736d5ea2df1cd
lauralorenz <llorenz@districtdatalabs.com>
2017-05-22 00:31:39 -0700

    leave this config up to the .env files; in any case redis is out

--- compose/django/entrypoint.sh
+++ compose/django/entrypoint.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 set -e
-# This entrypoint is used to play nicely with the current cookiecutter configuration.
-# Since docker-compose relies heavily on environment variables itself for configuration, we'd have to define multiple
-# environment variables just to support cookiecutter out of the box. That makes no sense, so this little entrypoint
-# does all this for us.
-export REDIS_URL=redis://redis:6379
-
-# the official postgres image uses 'postgres' as default user if not set explictly.
-if [ -z "$POSTGRES_USER" ]; then
-    export POSTGRES_USER=postgres
-fi
-
-# Default DB name is same as username, but allow it to be overridden
-: ${POSTGRES_DB:=$POSTGRES_USER}
-
-export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@postgres:5432/$POSTGRES_DB
 
 until psql -h postgres -U "postgres" -c '\l'; do
   >&2 echo "Postgres is unavailable - sleeping"
